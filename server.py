38037b677a607491590ead1dd7fc735117d78efb
Bala-Vignesh-S-M <vicky.bala.mg2002@gmail.com>
2023-09-05 21:32:37 +0530

    chNEGS

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify	from flask import Flask
+from flask import Flask, request, jsonify
 from dataclasses import dataclass
 
 @dataclass
@@ -6,30 +6,24 @@ class Result:
     result: int
 
 
-
-app = Flask(__name__)	app = Flask(__name__)
-
+app = Flask(__name__)
 
 
+@app.route("/calculator/greeting", methods=['GET'])
+def greeting():
+    return 'Hello world!'
 
-@app.route("/calculator/greeting", methods=['GET'])	@app.route("/calculator/greeting", methods=['GET'])
-def greeting():	def greeting():
-    return 'Hello world!'	    return ''
-
-
-@app.route("/calculator/add", methods=['POST'])	@app.route("/calculator/add", methods=['POST'])
-def add():	def add():
-    numbers = request.json	    return ''
+@app.route("/calculator/add", methods=['POST'])
+def add():
+    numbers = request.json
     response = Result(numbers['first'] + numbers['second'])
     return jsonify(response)
 
-
-@app.route("/calculator/subtract", methods=['POST'])	@app.route("/calculator/subtract", methods=['POST'])
-def subtract():	def subtract():
-    numbers = request.json	    return ''
+@app.route("/calculator/subtract", methods=['POST'])
+def subtract():
+    numbers = request.json
     response = Result(numbers['first'] - numbers['second'])
     return jsonify(response)
 
-
-if __name__ == '__main__':	if __name__ == '__main__':
+if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
